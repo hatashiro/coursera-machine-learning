814d7eb32970f31898d098cca92d299a8f133a83
Hyunje Jun <me@noraesae.net>
2018-09-09 14:26:33 +0900

    [ex1] Finish optional parts

--- ex1/computeCostMulti.m
+++ ex1/computeCostMulti.m
@@ -6,16 +6,11 @@ function J = computeCostMulti(X, y, theta)
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
-J = 0;
-
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
-
-
-
+J = sum((X * theta - y) .^ 2) / (2*m);
 
 % =========================================================================
 
--- ex1/normalEqn.m
+++ ex1/normalEqn.m
@@ -3,20 +3,12 @@ function [theta] = normalEqn(X, y)
 %   NORMALEQN(X,y) computes the closed-form solution to linear 
 %   regression using the normal equations.
 
-theta = zeros(size(X, 2), 1);
-
 % ====================== YOUR CODE HERE ======================
 % Instructions: Complete the code to compute the closed form solution
 %               to linear regression and put the result in theta.
 %
 
-% ---------------------- Sample Solution ----------------------
-
-
-
-
-% -------------------------------------------------------------
-
+theta = pinv(X' * X) * X' * y;
 
 % ============================================================
 
