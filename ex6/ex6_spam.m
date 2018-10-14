c730ccbfbde47a05e3f997cf7bc368cc0922987f
Hyunje Jun <me@noraesae.net>
2018-10-14 10:07:46 +0900

    [ex6] make the last part a loop

--- ex6/ex6_spam.m
+++ ex6/ex6_spam.m
@@ -125,14 +125,27 @@ pause;
 % Set the file to be read in (change this to spamSample2.txt,
 % emailSample1.txt or emailSample2.txt to see different predictions on
 % different emails types). Try your own emails as well!
-filename = 'spamSample1.txt';
 
-% Read and predict
-file_contents = readFile(filename);
-word_indices  = processEmail(file_contents);
-x             = emailFeatures(word_indices);
-p = svmPredict(model, x);
+files = {
+    'emailSample1.txt'
+    'emailSample2.txt'
+    'spamSample1.txt'
+    'spamSample2.txt'
+};
 
-fprintf('\nProcessed %s\n\nSpam Classification: %d\n', filename, p);
-fprintf('(1 indicates spam, 0 indicates not spam)\n\n');
+for i = 1:length(files)
+    filename = files{i}
 
+    % Read and predict
+    file_contents = readFile(filename);
+    word_indices  = processEmail(file_contents);
+    x             = emailFeatures(word_indices);
+    p = svmPredict(model, x);
+
+    fprintf('\nProcessed %s\n\nSpam Classification: %d\n', filename, p);
+    fprintf('(1 indicates spam, 0 indicates not spam)\n\n');
+
+    fprintf('\n\n');
+    fprintf('\nProgram paused. Press enter to continue.\n');
+    pause;
+end
