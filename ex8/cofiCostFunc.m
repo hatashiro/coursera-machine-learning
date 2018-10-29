cfd0bd8e6c4f95c22985c2805ff1cba5e0289cad
Hyunje Jun <me@noraesae.net>
2018-10-29 17:25:40 +0900

    [ex8] finish collaborative filtering

--- ex8/cofiCostFunc.m
+++ ex8/cofiCostFunc.m
@@ -11,7 +11,6 @@ X = reshape(params(1:num_movies*num_features), num_movies, num_features);
 Theta = reshape(params(num_movies*num_features+1:end), ...
                 num_users, num_features);
 
-            
 % You need to return the following values correctly
 J = 0;
 X_grad = zeros(size(X));
@@ -40,20 +39,11 @@ Theta_grad = zeros(size(Theta));
 %                     partial derivatives w.r.t. to each element of Theta
 %
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+J = sum(sum(((X * Theta' - Y) .* R) .^ 2)) / 2 ...
+    + lambda * sum(sum(X .^ 2)) / 2 ...
+    + lambda * sum(sum(Theta .^ 2)) / 2;
+X_grad = ((X * Theta' - Y) .* R) * Theta + lambda * X;
+Theta_grad = ((Theta * X' - Y') .* R') * X + lambda * Theta;
 
 % =============================================================
 
